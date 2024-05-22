e75e351ea9fc588f83251b4f83bb927b32c8b6c3
luoyi <luoyi58624@163.com>
2024-05-22 18:14:17 +0800

    feat: logger导出Level api

--- lib/luoyi_dart_base.dart
+++ lib/luoyi_dart_base.dart
@@ -10,6 +10,8 @@ import 'package:crypto/crypto.dart' as crypto;
 import 'package:logger/logger.dart';
 import 'package:uuid/uuid.dart';
 
+export 'package:logger/logger.dart' show Level;
+
 part 'src/model.dart';
 
 part 'src/util.dart';
@@ -17,3 +19,27 @@ part 'src/util.dart';
 part 'src/logger.dart';
 
 part 'src/uuid.dart';
+
+class DartBase {
+  DartBase._();
+
+  /// 设置日志打印实例
+  /// * level 显示的日志级别，如果为null，则在开发模式下显示所有的日志，生产模式下则屏蔽所有日志；
+  /// 如果指定了日志级别，那么在开发模式下和生产模式下将显示指定的日志级别之上的所有日志。
+  /// 例：
+  /// ``` dart
+  ///   // 开发、生产模式下显示info级别及以上的日志
+  ///   initLogger(level: Level.info);
+  ///
+  ///   // 开发模式下显示info级别及以上的日志，生产模式不显示日志
+  ///   initLogger(level: DartUtil.isRelease ? null : Level.info);
+  /// ```
+  /// * logger 完全自定义日志实例，请自行添加依赖包，本库仅导出 Level Api
+  void setLogger({Level? level, Logger? logger}) {
+    _logger = logger ??
+        Logger(
+          printer: _prettyPrinter,
+          filter: _LogFilter(loggerLevel: level),
+        );
+  }
+}
--- lib/src/logger.dart
+++ lib/src/logger.dart
@@ -12,26 +12,6 @@ var _logger = Logger(
   filter: _LogFilter(loggerLevel: null),
 );
 
-/// 自定义初始化日志
-/// * level 显示的日志级别，如果为null，则在开发模式下显示所有的日志，生产模式下则屏蔽所有日志；
-/// 如果指定了日志级别，那么在开发模式下和生产模式下将显示指定的日志级别之上的所有日志。
-/// 例：
-/// ``` dart
-///   // 开发、生产模式下显示info级别及以上的日志
-///   initLogger(level: Level.info);
-///
-///   // 开发模式下显示info级别及以上的日志，生产模式不显示日志
-///   initLogger(level: DartUtil.isRelease ? null : Level.info);
-/// ```
-/// * logger 自定义logger
-void initLogger({Level? level, Logger? logger}) {
-  _logger = logger ??
-      Logger(
-        printer: _prettyPrinter,
-        filter: _LogFilter(loggerLevel: level),
-      );
-}
-
 /// debug 级别日志输出
 void d(dynamic message, [dynamic title]) {
   _logger.d(message, error: title);
