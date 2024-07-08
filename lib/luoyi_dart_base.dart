aaa0e7975eeab20f2fe65f8f594681f8a22ee419
luoyi <luoyi58624@163.com>
2024-07-08 11:57:21 +0800

    feat: 新增platform工具类

--- lib/luoyi_dart_base.dart
+++ lib/luoyi_dart_base.dart
@@ -5,6 +5,7 @@ export 'package:logger/logger.dart' show Level;
 export 'src/global.dart';
 export 'src/logger.dart' show d, i, w, e;
 export 'src/util.dart';
+export 'src/platform/platform.dart';
 export 'src/extensions/map.dart';
 export 'src/extensions/num.dart';
 export 'src/extensions/fun.dart';
